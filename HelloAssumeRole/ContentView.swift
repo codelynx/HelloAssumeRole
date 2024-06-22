6ea9fcb32a70aa357393b8a41b107886ce70e687
Kaz Yoshikawa <kaz@digitallynx.com>
2024-06-22 16:08:34 -0400

    Initial Commit

---
+++ HelloAssumeRole/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HelloAssumeRole
+//
+//  Created by Kaz Yoshikawa on 2024/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HelloAssumeRole/HelloAssumeRoleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloAssumeRoleApp.swift
+//  HelloAssumeRole
+//
+//  Created by Kaz Yoshikawa on 2024/06/22.
+//
+
+import SwiftUI
+
+@main
+struct HelloAssumeRoleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
